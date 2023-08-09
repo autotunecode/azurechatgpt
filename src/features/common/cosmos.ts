653b9fb389dee440eb6806d7d3aff5579c51d319
kozuka <15242577+masaka27@users.noreply.github.com>
2023-08-09 11:12:47 +0900

    first-commit

--- src/features/common/cosmos.ts
+++ src/features/common/cosmos.ts
@@ -1,20 +1,20 @@
-"use server";
-import { CosmosClient } from "@azure/cosmos";
+'use server';
+import { CosmosClient } from '@azure/cosmos';
 
 // Read Cosmos DB_NAME and CONTAINER_NAME from .env
-const DB_NAME = process.env.AZURE_COSMOSDB_DB_NAME || "chat";
-const CONTAINER_NAME = process.env.AZURE_COSMOSDB_CONTAINER_NAME || "history";
+const DB_NAME = process.env.AZURE_COSMOSDB_DB_NAME || 'chat';
+const CONTAINER_NAME = process.env.AZURE_COSMOSDB_CONTAINER_NAME || 'history';
 
 export const initDBContainer = async () => {
   const endpoint = process.env.AZURE_COSMOSDB_URI;
   const key = process.env.AZURE_COSMOSDB_KEY;
   const client = new CosmosClient({ endpoint, key });
-  const databaseResponse = await client.databases.createIfNotExists({
+  const csdbdatabaseResponse = await client.databases.createIfNotExists({
     id: DB_NAME,
   });
 
   const containerResponse =
-    await databaseResponse.database.containers.createIfNotExists({
+    await csdbdatabaseResponse.database.containers.createIfNotExists({
       id: CONTAINER_NAME,
     });
 
